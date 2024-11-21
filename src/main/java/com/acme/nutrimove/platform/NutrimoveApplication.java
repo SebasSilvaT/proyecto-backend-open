7e7b5b8d344b42806f9e2d0af74d335f450339c8
SebasSilvaT <svsilvat@gmail.com>
2024-11-20 22:24:49 -0500

    commit 18-conectionT

--- src/main/java/com/acme/nutrimove/platform/NutrimoveApplication.java
+++ src/main/java/com/acme/nutrimove/platform/NutrimoveApplication.java
@@ -2,10 +2,6 @@ package com.acme.nutrimove.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class NutrimoveApplication {
@@ -14,16 +10,4 @@ public class NutrimoveApplication {
         SpringApplication.run(NutrimoveApplication.class, args);
     }
 
-    @Configuration
-    public static class WebConfig implements WebMvcConfigurer {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/api/v1/**")
-                    .allowedOriginPatterns("*")
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true);
-
-        }
-    }
 }
