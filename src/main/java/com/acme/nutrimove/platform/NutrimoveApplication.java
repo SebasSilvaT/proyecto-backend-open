0dfb5d15d245bea491845286b7ccd2a1245d1875
SebasSilvaT <svsilvat@gmail.com>
2024-11-20 23:06:38 -0500

    commit 19-video

--- src/main/java/com/acme/nutrimove/platform/NutrimoveApplication.java
+++ src/main/java/com/acme/nutrimove/platform/NutrimoveApplication.java
@@ -2,6 +2,7 @@ package com.acme.nutrimove.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,17 +15,16 @@ public class NutrimoveApplication {
     }
 
     @Configuration
-    public static class WebConfig implements WebMvcConfigurer {
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/api/v1/**") // Endpoints que permiten CORS
-                    .allowedOrigins(
-                            "https://673e677494f3a8000828c568--nutrimove.netlify.app", // Dominio del frontend
-                            "https://nutrimove.up.railway.app" // Dominio del backend para pruebas
-                    )
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                    .allowedHeaders("*") // Permitir todas las cabeceras
-                    .allowCredentials(true); // Para manejo de sesiones o cookies
+    public static class Myconfiguration{
+        @Bean
+        public WebMvcConfigurer corsConfigurer() {
+            return new WebMvcConfigurer() {
+                @Override
+                public void addCorsMappings(CorsRegistry registry) {
+                    registry.addMapping("/**")
+                            .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH");
+                }
+            };
         }
     }
 }
